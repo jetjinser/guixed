93e28e3b251b30e13ca1c5df75f7305e08bcbf8f
Jinser Kafka <aimer@purejs.icu>
2025-06-22 05:07:30 +0800

    meta(channel): pull

--- channels.scm
+++ channels.scm
@@ -1,11 +1,22 @@
 (list (channel
         (name 'guix)
-        (url "https://codeberg.org/guix/guix-mirror.git")
+        (url "https://codeberg.org/guix/guix.git")
         (branch "master")
         (commit
-          "97ea59b846c5267098a019f36c84dcaa55fb123e")
+          "703998a6e60f34231bf59866bd8bef7be516754a")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rosenthal)
+        (url "https://codeberg.org/hako/rosenthal.git")
+        (branch "trunk")
+        (commit
+          "aa858e4b87e76ce5aece6ed4f93a87695f8f0776")
+        (introduction
+          (make-channel-introduction
+            "7677db76330121a901604dfbad19077893865f35"
+            (openpgp-fingerprint
+              "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
