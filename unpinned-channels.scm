9a22349326e713016dfa08117ec85f0854f4aaec
Jinser Kafka <aimer@purejs.icu>
2025-06-22 05:01:35 +0800

    meta(channel): add rosenthal

--- unpinned-channels.scm
+++ unpinned-channels.scm
@@ -6,4 +6,13 @@
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rosenthal)
+        (url "https://codeberg.org/hako/rosenthal.git")
+        (branch "trunk")
+        (introduction
+          (make-channel-introduction
+           "7677db76330121a901604dfbad19077893865f35"
+           (openpgp-fingerprint
+            "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
