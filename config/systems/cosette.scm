89236bc8727b835f69cf59b04c76bf5bd16b2497
Jinser Kafka <aimer@purejs.icu>
2025-11-19 11:12:36 +0800

    meta(substituter): add guix.more

--- config/systems/cosette.scm
+++ config/systems/cosette.scm
@@ -92,7 +92,17 @@
                                                  (readymedia-media-directory (path "/srv/store/v")
                                                                              (types '(V)))))))
                               (service tailscale-service-type
-                                       (tailscale-configuration)))
+                                       (tailscale-configuration))
+                              (simple-service 'guix-moe guix-service-type
+                                (guix-extension
+                                  (authorized-keys
+                                   (list (plain-file "guix-moe-old.pub"
+                                           "(public-key (ecc (curve Ed25519) (q #374EC58F5F2EC0412431723AF2D527AD626B049D657B5633AAAEBC694F3E33F9#)))")
+                                         ;; 2025-10-29 起用的新公鑰
+                                         (plain-file "guix-moe.pub"
+                                           "(public-key (ecc (curve Ed25519) (q #552F670D5005D7EB6ACF05284A1066E52156B51D75DE3EBD3030CD046675D543#)))")))
+                                  (substitute-urls
+                                   '("https://cache-cdn.guix.moe")))))
                         (modify-services %base-services
                                          (sysctl-service-type
                                            config =>
