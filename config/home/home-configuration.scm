8c09f0dabe3c64d085ee65852ba39e679f731790
Jinser Kafka <aimer@purejs.icu>
2026-05-21 12:38:47 +0800

    home: direnv and ssh-agent

--- config/home/home-configuration.scm
+++ config/home/home-configuration.scm
@@ -2,22 +2,30 @@
   #:use-module (gnu home)
   #:use-module (gnu home services)
   #:use-module (gnu home services shells)
+  #:use-module (gnu home services ssh)
   #:use-module (gnu packages)
   #:use-module (gnu packages vim)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages tmux)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages shellutils)
+  #:use-module (gnu packages bittorrent)
+  #:use-module ((gnu packages rust-apps) #:prefix rust:)
+  #:use-module (rosenthal services shellutils)
   #:use-module (gnu services)
-  #:use-module (guix gexp)
-  #:use-module (config packages transmission))
+  #:use-module (guix gexp))
 
 (home-environment
   (packages (list neovim git tmux curl
-                  transmission* direnv))
+                  transmission direnv
+                  rust:fd rust:ripgrep rust:bat))
   (services (list
               (service home-fish-service-type
                 (home-fish-configuration
                  (abbreviations '(("g" . "git")))))
+              (service home-fish-plugin-direnv-service-type)
               (service home-xdg-configuration-files-service-type
-                (list `("git/config" ,(local-file "../../files/gitconfig")))))))
+                (list `("git/config" ,(local-file "../../files/gitconfig"))))
+              (service home-ssh-agent-service-type
+                       (home-ssh-agent-configuration
+                        (extra-options '("-t" "1h30m")))))))
